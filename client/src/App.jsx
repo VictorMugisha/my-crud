257d2aeebc6563c2d1ae45363b9977281aa0a3e9
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 19:05:27 +0200

    Added required components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import './App.css'
 import Users from "./pages/Users"
+import CreateUser from "./pages/CreateUser"
+import UpdateUser from "./pages/UpdateUser"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Users />} />
-        
+        <Route path="/create" element={<CreateUser />} />
+        <Route path="/update/:id" element={<UpdateUser />} />
       </Routes>
     </BrowserRouter>
   )
