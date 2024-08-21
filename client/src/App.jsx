d9bc0271a70f9aa60cd9f4107f70b95d72c0f784
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 19:38:20 +0200

    Added a Navigation bar on the top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
 import './App.css';
 import Users from "./pages/Users";
 import CreateUser from "./pages/CreateUser";
@@ -8,6 +8,27 @@ export default function App() {
   return (
     <main className="app-container">
       <BrowserRouter>
+        <nav className="navbar">
+          <h1 className="navbar-brand">User Management</h1>
+          <ul className="navbar-links">
+            <li>
+              <NavLink
+                to="/"
+                className={({ isActive }) => isActive ? 'active-link' : 'link'}
+              >
+                Users
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/create"
+                className={({ isActive }) => isActive ? 'active-link' : 'link'}
+              >
+                Create User
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
         <Routes>
           <Route path="/" element={<Users />} />
           <Route path="/create" element={<CreateUser />} />
