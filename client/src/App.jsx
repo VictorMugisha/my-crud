dcdd26ae95ea17a4e54b74393a32adfcc0c8d239
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-21 19:23:43 +0200

    Added Poppins google fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,19 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-
-import './App.css'
-import Users from "./pages/Users"
-import CreateUser from "./pages/CreateUser"
-import UpdateUser from "./pages/UpdateUser"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+import Users from "./pages/Users";
+import CreateUser from "./pages/CreateUser";
+import UpdateUser from "./pages/UpdateUser";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/create" element={<CreateUser />} />
-        <Route path="/update/:id" element={<UpdateUser />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <main className="app-container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/create" element={<CreateUser />} />
+          <Route path="/update/:id" element={<UpdateUser />} />
+        </Routes>
+      </BrowserRouter>
+    </main>
+  );
 }
-
