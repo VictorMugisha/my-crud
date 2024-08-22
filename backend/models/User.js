a76ea76c338ad3320c6ea5befe99831a641355b3
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-22 09:34:16 +0200

    Create UserSchema

---
+++ backend/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+    }
+});
+
+module.exports = mongoose.model("User", UserSchema);
