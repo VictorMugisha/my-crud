98a23dce59202f8ffa0499fbe90594b1cbfc5aa3
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-27 19:32:07 +0200

    Added all required controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,8 +33,40 @@ async function createUser(req, res) {
   }
 }
 
+// Updating a user
+async function updateUser(req, res) {
+  const { id } = req.params;
+  const body = req.body;
+  try {
+    const user = await UserModel.findByIdAndUpdate(id, body);
+    res.status(200).json(user);
+  } catch (error) {
+    res
+      .status(400)
+      .json({ error, message: "Failed to find and update a user" });
+  }
+}
+
+// Deleting a user
+async function deleteUser(req, res) {
+  const { id } = req.params;
+  try {
+    const user = await UserModel.findById(id);
+    if (!user) {
+      return res.status(404).send("User not found");
+    }
+    await UserModel.deleteOne(user);
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    console.error(error); // log the error on the server-side
+    res.status(500).send({ message: "Failed to delete user" });
+  }
+};
+
 module.exports = {
   getAllUsers,
   getSingleUser,
   createUser,
+  updateUser,
+  deleteUser
 };
