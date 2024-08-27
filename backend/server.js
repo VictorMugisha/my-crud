203d58302a83c7720b5c14bf86ec349226356650
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-27 19:37:27 +0200

    Used userRoutes from server file

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes.js");
 
-const UserModel = require("./models/User.js");
 const { default: mongoose } = require("mongoose");
 
 const app = express();
@@ -26,66 +26,4 @@ mongoose
   })
   .catch((error) => console.log(error));
 
-// Getting all users
-app.get("/", async (req, res) => {
-  try {
-    const users = await UserModel.find();
-    res.status(201).json(users);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// Getting a single user
-app.get("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await UserModel.findById(id);
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// Creating a new user
-app.post("/create", async (req, res) => {
-  const body = req.body;
-  try {
-    const user = new UserModel(body);
-    await user.save();
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
-});
-
-// Updating a user
-app.put("/:id", async (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  try {
-    const user = await UserModel.findByIdAndUpdate(id, body);
-    res.status(200).json(user);
-  } catch (error) {
-    res
-      .status(400)
-      .json({ error, message: "Failed to find and update a user" });
-  }
-});
-
-// Deleting a user
-app.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const user = await UserModel.findById(id);
-    if (!user) {
-      return res.status(404).send("User not found");
-    }
-    // await user.remove();
-    await UserModel.deleteOne(user);
-    res.status(200).send("User deleted successfully");
-  } catch (error) {
-    console.error(error); // log the error on the server-side
-    res.status(500).send({ message: "Failed to delete user" });
-  }
-});
+app.use("/api/users", userRoutes);
