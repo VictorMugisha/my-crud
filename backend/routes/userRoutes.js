75a8ae639c67ddef23367358b6beb649e495244b
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-08-27 19:34:10 +0200

    Defined all controllers on router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { getAllUsers } = require("../controllers/userController.js");
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+} = require("../controllers/userController.js");
 
 router.get("/", getAllUsers);
+router.get("/:id", getSingleUser);
+router.post("/create", createUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
 
 module.exports = router;
